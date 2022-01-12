a708ce7b1106ea1bc08493ab34fd5a7049ce8c6b
Shishir Ghate <Shishir.Ghate@dowjones.com>
2022-01-12 09:58:24 -0600

    Slow down the intake

--- TeamCode/src/main/java/com/edinaftc/library/subsystems/Intake.java
+++ TeamCode/src/main/java/com/edinaftc/library/subsystems/Intake.java
@@ -14,16 +14,14 @@ public class Intake extends Subsystem {
 
     public Intake(HardwareMap map){
         _motor4321 = map.get(DcMotorEx.class,"intake");
-        _motor4321.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        _motor4321.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
     public void update(){
         if (_direction == Direction.Counterclockwise){
-            _motor4321.setPower(.05);
+            _motor4321.setPower(.5);
         } else if (_direction == Direction.Clockwise){
-            _motor4321.setPower(-.05);
+            _motor4321.setPower(-.5);
         } else {
             _motor4321.setPower(0);
         }
